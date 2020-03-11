ff3b95f330c53218a9b9f6efaadaa349274e30cb
Voyz <18217501371@163.com>
2020-03-11 15:50:20 +0800

    我的页面

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -15,6 +15,11 @@ App({
       })
     }
 
+    wx.getUserInfo(res=>{
+      console.log(res)
+    })
+
+    console.log('hhhhh')
     this.globalData = {}
   }
 })
