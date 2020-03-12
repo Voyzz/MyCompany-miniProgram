8e553e4995dbc7da5f3ca7fbf1d2f9b4d5ec2a71
Voyz <18217501371@163.com>
2020-03-12 14:39:25 +0800

    我的页面业务宫格

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,7 +3,38 @@ const app = getApp()
 
 Page({
   data: {
-    
+    squareList:[
+      {
+        icon:"/images/icons/market.png",
+        title:"我的订单",
+        jumpUrl:''
+      },
+      {
+        icon:"/images/icons/likes.png",
+        title:"我的收藏",
+        jumpUrl:''
+      },
+      {
+        icon:"/images/icons/history.png",
+        title:"浏览历史",
+        jumpUrl:''
+      },
+      {
+        icon:"/images/icons/info.png",
+        title:"个人信息",
+        jumpUrl:''
+      },
+      {
+        icon:"/images/icons/phone.png",
+        title:"联系我们",
+        jumpUrl:''
+      },
+      {
+        icon:"/images/icons/alert.png",
+        title:"最新动态",
+        jumpUrl:''
+      },
+    ]
   },
 
   onLoad: function() {
