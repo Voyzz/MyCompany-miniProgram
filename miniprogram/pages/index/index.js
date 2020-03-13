67e7b4b565c749d39d0f7a967f906a2450ff1cc9
Voyz <18217501371@163.com>
2020-03-13 11:26:13 +0800

    背景色PS

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,12 +3,39 @@ const app = getApp()
 
 Page({
   data: {
+    orderInfo:{
+      icon:"/images/icons/market.png",
+      title:"我的订单",
+      jumpUrl:'',
+      orderItemColor:'#9c9c9c',
+      orderItemsList:[
+        {
+          icon:'cash-back-record',
+          title:"待支付",
+          jumpUrl:'',
+          info:""
+        },
+        {
+          icon:'logistics',
+          title:"进行中",
+          jumpUrl:'',
+          info:"9"
+        },
+        {
+          icon:'completed',
+          title:"已完成",
+          jumpUrl:'',
+          info:""
+        },
+        {
+          icon:'failure',
+          title:"已取消",
+          jumpUrl:'',
+          info:"2"
+        }
+      ]
+    },
     squareList:[
-      {
-        icon:"/images/icons/market.png",
-        title:"我的订单",
-        jumpUrl:''
-      },
       {
         icon:"/images/icons/likes.png",
         title:"我的收藏",
