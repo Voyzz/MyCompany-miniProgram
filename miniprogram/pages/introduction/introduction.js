6ef2de12a2d870b4cf8f9ea550328569d49c07f4
Voyz <18217501371@163.com>
2020-03-16 15:49:16 +0800

    公司简介页

--- miniprogram/pages/introduction/introduction.js
+++ miniprogram/pages/introduction/introduction.js
@@ -1,66 +1,41 @@
 // miniprogram/pages/introduction/introduction.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 头图
+    intro_banner:"http://voyz-image.test.upcdn.net/2020/03/16/731c0dda1de9ab23503562b9fa8b03f2.png",
+    // 公司简介
+    intro_info:{
+      title:"关于我们",
+      subtitle:"ABOUT US",
+      content:"关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们,关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们。"
+    },
+    // 公司地址
+    intro_address:{
+      title:"公司地址",
+      subtitle:"COMPANY ADDRESS"
+    },
+    // 地图参数
+    map:{
+      longitude:"113.8141214800",
+      latitude:"35.3262077200",
+      scale:"16"
+    },
+    markers: [{
+      id: 0,
+      latitude: 35.3262077200,
+      longitude: 113.8141214800,
+      // width: 50,
+      // height: 50,
+      label:{
+        content:"中帆东盛",
+        bgColor:"#fff",
+        padding:5,
+        borderRadius:5,
+        color:"#679fe4"
+      }
+    }],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
