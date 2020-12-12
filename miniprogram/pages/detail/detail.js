aabd40db83f1d12d9b8938f336a56adcad919a95
Voyz <18217501371@163.com>
2020-12-12 20:48:55 +0800

    暂存

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -5,62 +5,99 @@ Page({
    * 页面的初始数据
    */
   data: {
-    prod_img:'https://img.yzcdn.cn/vant/cat.jpeg'
+    porduct_info:{
+      pro_id:'pro_111111',
+      title:'商品标题',
+      substitle:'Production Subtitle',
+      is_favorite:false,
+      is_top:false,
+      extra_flag:false,
+      tags_list:[],
+      class_list:['热门','液态'],
+      cover_img: {
+        img_url:'https://img.yzcdn.cn/vant/cat.jpeg',
+        is_show:true,
+        img_id:'img_11111'
+      },
+      pro_image_list:[
+        {
+          pro_id:'pro_111111',
+          img_url:'https://img.yzcdn.cn/vant/cat.jpeg',
+          is_show:true,
+          img_id:'img_11111'
+        },
+        {
+          pro_id:'pro_111111',
+          img_url:'https://img.yzcdn.cn/vant/cat.jpeg',
+          is_show:true,
+          img_id:'img_2222'
+        },
+        {
+          pro_id:'pro_111111',
+          img_url:'https://img.yzcdn.cn/vant/cat.jpeg',
+          is_show:true,
+          img_id:'img_3333'
+        }
+      ],
+      pro_detail_list:[
+        {
+          pro_id:'pro_111111',
+          type:'info',
+          name:'产品简介',
+          content:"这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介",
+          detail_obj:{},
+          detail_arr:[]
+        },
+        {
+          pro_id:'pro_111111',
+          type:'info',
+          name:'产品参数',
+          content:"",
+          detail_obj:[
+            {
+              key:'参数1',
+              value:'xxxx'
+            },
+            {
+              key:'参数2',
+              value:'xxxx'
+            },
+            {
+              key:'参数3',
+              value:'xxxx'
+            }
+          ],
+          detail_arr:[]
+        },
+        {
+          pro_id:'pro_111111',
+          type:'info',
+          name:'主要用途',
+          content:"",
+          detail_obj:{},
+          detail_arr:['用途1','用途2','用途3']
+        }
+      ],
+      price_list:[
+        {
+          pro_id:'pro_111111',
+          date:'2020-12-20',
+          price:'10000',
+          unit:'吨',
+          currency:'¥',
+          is_recent:true
+        }
+      ]
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
+    if(!!options.news_data) {
+      console.log(JSON.parse(options.news_data) );
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- miniprogram/pages/homepage/homepage.js
+++ miniprogram/pages/homepage/homepage.js
@@ -3,21 +3,20 @@
 
 Page({
   data: {
-    modules_data:[]
+    modules_data:[],
+    show_popup:false,
+    curr_popup_content:''
   },
 
   onLoad: function (options) {
     const { baseUrl } = getApp();
     const _this = this;
 
+    // 请求接口
     wx.request({
       url: `${baseUrl}/homepageModules`,
       method: "POST",
       data:{
-        // moduleType:'header',
-        // pType:'update',
-        // moduleData:JSON.stringify(_data),
-        // position:5,
         onShow:1,
       },
       success(res){
@@ -28,6 +27,41 @@ Page({
           })
         }
       }
+    })
+  },
+
+  // ************ 控制popup 
+  show_news_popup : function() {
+    this.setData({
+      curr_popup_content:'news',
+      show_popup:true
+    })
+  },
+
+  show_favorite_popup : function() {
+    this.setData({
+      curr_popup_content:'favorite',
+      show_popup:true
+    })
+  },
+
+  show_contact_popup : function() {
+    this.setData({
+      curr_popup_content:'contact',
+      show_popup:true
+    })
+  },
+  onClose:function (params) {
+    this.setData({
+      show_popup:false,
+    })
+  },
+
+  // 点击新闻卡片
+  click_news_card : function (params) {
+    const {news_data} =  params.currentTarget.dataset;
+    wx.navigateTo({
+      url: '/pages/detail/detail?news_data='+JSON.stringify(news_data),
     })
   },
 
